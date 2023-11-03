763222d13d1102cd0448edee7357d36ce31f71c8
Keydol <45651225+Keydol@users.noreply.github.com>
2023-11-03 22:38:56 +0200

    Create t.js

---
+++ t.js
@@ -0,0 +1,232 @@
+[
+    {
+        "id": 9,
+        "key": "1358c57f9fbcea28c182c7e61639c254",
+        "name": "Bear killer",
+        "diamonds": 3,
+        "next_available": true,
+        "instructions": "Kill 10 bears",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 6,
+        "key": "level_10",
+        "name": "Level 25",
+        "diamonds": 4,
+        "next_available": null,
+        "instructions": "Get to level 25",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 2,
+        "key": "031c23f9c9938469dccc5f3c10c2c07a",
+        "name": "Level 50",
+        "diamonds": 5,
+        "next_available": null,
+        "instructions": "Get to level 50",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 4,
+        "key": "93b83485c97b10d8822888c1b81e9715",
+        "name": "Dragon Killer",
+        "diamonds": 5,
+        "next_available": null,
+        "instructions": "Kill 1 Dragon!",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 5,
+        "key": "server_leader",
+        "name": "Leaderboard #1",
+        "diamonds": 5,
+        "next_available": null,
+        "instructions": "Collect enough gold to be #1 on the server",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 13,
+        "key": "6d4306be19312ef8178630a10d8de5b4",
+        "name": "10 Kills reward",
+        "diamonds": 5,
+        "next_available": null,
+        "instructions": "Kill 10 enemy",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 14,
+        "key": "595c01bcacef942c2975cb86c0fe41a0",
+        "name": "Wood Breaker",
+        "diamonds": 5,
+        "next_available": null,
+        "instructions": "Destroy 50 wood blocks",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 19,
+        "key": "2d9d6c5cb367215eeca418dbf50424f7",
+        "name": "Snow Gladiator",
+        "diamonds": 5,
+        "next_available": null,
+        "instructions": "Kill 25 White Walkers",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 24,
+        "key": "090fd2eee3471a575d8bda47f3280f7d",
+        "name": "Share Glar.io",
+        "diamonds": 5,
+        "next_available": null,
+        "instructions": "Check the store tab, share your link to earn diamonds!",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 25,
+        "key": "9bb73eba28769d5c4fd3329733d4fa6c",
+        "name": "Spider Killer",
+        "diamonds": 5,
+        "next_available": null,
+        "instructions": "Hunt a spider, and kill it!",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 26,
+        "key": "9ba974e2fd3a27e50c7a475b74153158",
+        "name": "Rabbit Chaser",
+        "diamonds": 5,
+        "next_available": null,
+        "instructions": "Hunt and kill 5 rabbits!",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 21,
+        "key": "0e92a122ce6fc279a9bd1767a5306335",
+        "name": "Bacon Maker",
+        "diamonds": 6,
+        "next_available": null,
+        "instructions": "Kill 50 Pigs",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 7,
+        "key": "81be66ef4451b5dbb3e37e120658641c",
+        "name": "Chicken Chaser",
+        "diamonds": 8,
+        "next_available": null,
+        "instructions": "Kill 50 chickens",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 8,
+        "key": "f2b9573d1a3eb738b82f6fa5e8803c46",
+        "name": "Wolf Slayer",
+        "diamonds": 8,
+        "next_available": null,
+        "instructions": "Kill 50 wolves",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 16,
+        "key": "29dbe41960cba9d8faa1139b88488476",
+        "name": "100,000 Gold",
+        "diamonds": 8,
+        "next_available": null,
+        "instructions": "Reach 100,000 Gold",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 20,
+        "key": "9d579745ff3c4be57caf96fa80c8067d",
+        "name": "Peasant bully",
+        "diamonds": 8,
+        "next_available": null,
+        "instructions": "Kill 200 Peasants",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 23,
+        "key": "41e63214a42d9f74d0d70bd9bdc27bd6",
+        "name": "10 day streak",
+        "diamonds": 10,
+        "next_available": null,
+        "instructions": "Play for 10 days in a row!",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 3,
+        "key": "leaderboard",
+        "name": "Leaderboard",
+        "diamonds": 20,
+        "next_available": null,
+        "instructions": "Collect enough gold to get on the weekly leaderboard",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 15,
+        "key": "56bcabcca62aa3b2386cbaeea47d8e3f",
+        "name": "Level 10,000 Extreme",
+        "diamonds": 20,
+        "next_available": null,
+        "instructions": "Get to level 10,000 (Extreme)",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 17,
+        "key": "254c7773a812ee97c20e90624311f415",
+        "name": "1 Million Gold",
+        "diamonds": 20,
+        "next_available": null,
+        "instructions": "Get 1 million gold",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 1,
+        "key": "leaderboard_1",
+        "name": "Leaderboard #1",
+        "diamonds": 25,
+        "next_available": null,
+        "instructions": "Collect enough gold to be #1 on the weekly leaderboard",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 18,
+        "key": "b2598d4d2be3ec05e88e39c696d4da22",
+        "name": "100 Kills reward",
+        "diamonds": 30,
+        "next_available": null,
+        "instructions": "Kill 100 enemy",
+        "count": 0,
+        "next_available_in": 0
+    },
+    {
+        "id": 22,
+        "key": "cbcaa339e7021765cd427466581d808b",
+        "name": "YouTube Badge",
+        "diamonds": 50,
+        "next_available": null,
+        "instructions": "Check out our discord for how to contribute and earn a YouTube badge!",
+        "count": 0,
+        "next_available_in": 0
+    }
+]
